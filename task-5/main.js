4a0bbfb0dac0ee9f0dad0e1aed2bac09efd46ad4
ToghrulShukurlu <106272453+ToghrulShukurlu@users.noreply.github.com>
2022-06-21 20:48:46 +0400

    Update main.js

--- task-5/main.js
+++ task-5/main.js
@@ -5,6 +5,7 @@ prompt.start();
 prompt.get('input', function (err, result) {
 
    var n = parseInt(result.input);
+   var m = parseInt(result.input);
   for(i=n ; i<=m ; i++){
     if(i % 2 !==0)
     console.log(i);
