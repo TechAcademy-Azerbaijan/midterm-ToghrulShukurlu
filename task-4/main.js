3ff0018c6523105ea299f24673fbd50c670eab47
ToghrulShukurlu <106272453+ToghrulShukurlu@users.noreply.github.com>
2022-06-21 19:16:41 +0400

    Update main.js

--- task-4/main.js
+++ task-4/main.js
@@ -4,6 +4,9 @@ prompt.start();
 
 prompt.get('input', function (err, result) {
   
-    // Write code here
+    for(i=0 ; i<n ; i++){
+    if(n % i ==0 && i % 2 !==0)
+    console.log(i);
+}
   
 });
