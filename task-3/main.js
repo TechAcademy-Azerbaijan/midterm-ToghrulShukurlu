42b884fb65976bd949c91957dc8cd0506557c708
ToghrulShukurlu <106272453+ToghrulShukurlu@users.noreply.github.com>
2022-06-21 19:42:07 +0400

    Update main.js

--- task-3/main.js
+++ task-3/main.js
@@ -4,6 +4,19 @@ prompt.start();
 
 prompt.get('input', function (err, result) {
   
-    // Write code here
+    let n = ;
+
+const e = n % 10;
+const d = parseInt(n / 10) % 10 ;
+const c = parseInt(n / 100) % 10;
+const b = parseInt(n / 1000) % 10;
+const a = parseInt(n / 10000) % 10;
+console.log(a);
+
+if(a==b || a==c || a==d || a==e || b==c || b==d || b==e || c==d || c==e || d == e  ){
+console.log('YES');
+}else{
+    console.log('NO');
+}
   
 });
